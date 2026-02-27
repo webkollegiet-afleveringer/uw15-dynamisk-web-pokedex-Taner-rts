c7cf34e7f3547dcc80dc66179ae63ffa74841622
TJ <tjshotta24@gmail.com>
2026-02-27 14:23:27 +0100

    half way

--- detail.js
+++ detail.js
@@ -5,18 +5,53 @@ const mainDom = document.querySelector("main");
 const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
 
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    createPage(data)
-  })
+async function getData() {
+  const [pokemon, species] = await Promise.all([
+    fetch(url).then(result => result.json()),
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then(result => result.json())
+  ]);
 
-function createPage(data) {
+  console.log(pokemon);
+  console.log(species);
+  createPage(pokemon, species)
+}
+
+getData()
+
+// fetch(url)
+//   .then(res => res.json())
+//   .then(data => {
+//     createPage(data)
+//   })
+
+function createPage(data, species) {
   console.log(data);
 
+
+
+  const abilitiesType = data.types.map(abilityTypes => abilityTypes.type.name).join(", ");
+
+
+  const abilitiesStrong = data.abilities.map(abilityKey => abilityKey.ability.name).join(", ");
+
+  const abilitiesNumber = data.stats.map(abilityNumbs => abilityNumbs.base_stat).join(", ");
+
+  const textPokemon = species.flavor_text_entries.map(textInsert => textInsert.flavor_text).join(", ");
+  console.log(textPokemon);
+
+
   const contentString = `<h1>${data.name}</h1>
-  <p>weight : ${data.weight}</p>
   <img src="${artworkUrl}${id}.png" alt="${name}">
+  <p>${abilitiesType}</p>  
+  <h2> About </h2>
+  <p>Weight : ${data.weight}</p>
+  <p>Height : ${data.height}</p>
+  <p> Moves:${abilitiesStrong}</p>
+  
+  <ul> Base Stats </ul>
+  <li> ${abilitiesNumber} </li>
+  
+  <p> Moves:${textPokemon}</p>
   
   `
   mainDom.insertAdjacentHTML("beforebegin", contentString)
