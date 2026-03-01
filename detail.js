9cc1593599ef78424418d23d8036dc9808922ae1
TJ <tjshotta24@gmail.com>
2026-03-01 02:46:50 +0100

    css in the making

---
+++ detail.js
@@ -0,0 +1,58 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const mainDom = document.querySelector("main");
+const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+
+async function getData() {
+  const [pokemon, species] = await Promise.all([
+    fetch(url).then(result => result.json()),
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then(result => result.json())
+  ]);
+
+  console.log(pokemon);
+  console.log(species);
+  createPage(pokemon, species)
+}
+
+getData()
+
+// fetch(url)
+//   .then(res => res.json())
+//   .then(data => {
+//     createPage(data)
+//   })
+
+function createPage(data, species) {
+  console.log(data);
+
+
+
+  const abilitiesType = data.types.map(abilityTypes => abilityTypes.type.name).join(", ");
+
+
+  const abilitiesStrong = data.abilities.map(abilityKey => abilityKey.ability.name).join(", ");
+
+  const abilitiesNumber = data.stats.map(abilityNumbs => abilityNumbs.base_stat).join(", ");
+  // console.log(abilitiesNumber);
+  //   const textPokemon = species.flavor_text_entries.map(textInsert => textInsert.flavor_text).join(", ");
+  //   
+
+
+  const contentString = `<h1>${data.name}</h1>
+  <img src="${artworkUrl}${id}.png" alt="${name}">
+  <p>${abilitiesType}</p>  
+  <h2> About </h2>
+  <p>Weight : ${data.weight}</p>
+  <p>Height : ${data.height}</p>
+  <p> Moves:${abilitiesStrong}</p>
+  
+  <ul> Base Stats </ul>
+  <li> ${abilitiesNumber} </li>
+  
+
+  
+  `
+  mainDom.insertAdjacentHTML("beforebegin", contentString)
+}
---
+++ liste.js
@@ -0,0 +1,80 @@
+let currentOffset = 0;
+let limit = 20;
+const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+
+
+function fetchPokemon(offset) {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+
+        .then(response => response.json())
+        .then(data => {
+
+            displayPokemon(data);
+        })
+}
+
+const mainDom = document.querySelector("main");
+function displayPokemon(data) {
+    const results = data.results
+    const pokemonsString = results.map((result) => {
+        const { name, url } = result
+        const id = getIdFromPokemon(url)
+
+        return /* html */`
+        <article>
+        <a href="detail.html?id=${id}">
+        
+        <p>##${id}</p>
+        
+        
+        
+        <img src="${artworkUrl}${id}.png" alt="${name}">
+        <h2>${name}</h2>
+        
+        </a>
+        </article>
+
+        `
+    }).join("")
+
+
+    mainDom.insertAdjacentHTML("beforeend", pokemonsString);
+
+    let observedPokemon = document.querySelector("main article:nth-last-child(10)")
+    observedPokemon.classList.add("red")
+    observer.observe(observedPokemon)
+}
+
+function getIdFromPokemon(PokemonUrl) {
+    return PokemonUrl.slice(0, -1).split("/").pop()
+
+
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            currentOffset = currentOffset + 20
+            console.log(currentOffset);
+
+            if (currentOffset < 1330) {
+                observer.unobserve(entry.target)
+                fetchPokemon(currentOffset)
+
+            }
+            else {
+                console.log("End");
+            }
+        }
+    })
+
+
+}, {
+    threshold: 1
+})
+
+
+
+
+fetchPokemon(currentOffset)
